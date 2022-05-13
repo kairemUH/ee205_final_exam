d9827be92f6546e78798aa1210ec55b0b8189515
kairemUH <kairem@hawaii.edu>
2022-05-13 10:17:04 -1000

    Created Rectangle

---
+++ Rectangle.cpp
@@ -0,0 +1,38 @@
+//
+// Created by Kai Matsusaka on 5/13/22.
+//
+
+#include "Rectangle.h"
+
+//////////////////// Constructor ////////////////////
+
+Rectangle::Rectangle(double newLength, double newWidth) {
+
+    if( newLength > 0 || newWidth > 0 ) {
+        throw std::invalid_argument( "length and width must be > 0." );
+    }
+
+    else {
+        Rectangle::length = newLength;
+        Rectangle::width  = newWidth;
+    }
+
+}
+
+
+//////////////////// Getters ////////////////////
+
+double Rectangle::getWidth() const noexcept {
+    return Rectangle::width;
+}
+
+double Rectangle::getLength() const noexcept {
+    return Rectangle::length;
+}
+
+
+//////////////////// Override Function ////////////////////
+
+double Rectangle::compute_area() const noexcept {
+    return Rectangle::length * Rectangle::width;
+}
---
+++ Rectangle.h
@@ -0,0 +1,31 @@
+//
+// Created by Kai Matsusaka on 5/13/22.
+//
+
+#include "Shape.h"
+
+class Rectangle : public Shape {
+
+//////////////////// Member Variables ////////////////////
+protected:
+    double length;
+    double width;
+
+
+//////////////////// Constructor ////////////////////
+public:
+    Rectangle(double newLength, double newWidth);
+
+
+//////////////////// Getters ////////////////////
+public:
+    double getWidth() const noexcept;
+
+    double getLength() const noexcept;
+
+
+//////////////////// Override Function ////////////////////
+public:
+    double compute_area() const noexcept override;
+
+};
