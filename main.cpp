ce87c543da23a98163917b05fd1b0370dc3a7e9d
kairemUH <kairem@hawaii.edu>
2022-05-13 10:28:24 -1000

    Refactored Main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include "Square.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "Yo" << std::endl;
+
+    Square mySquare(2);
+    std::cout << "The area of mySquare is: "
+              << mySquare.compute_area()
+              << std::endl;
+    Rectangle myRectangle( 2, 4 );
+    std::cout << "The area of myRectangle is: "
+              << myRectangle.compute_area()
+              << std::endl;
+
     return 0;
 }
